a50c8b1e677c88422861f795c63c388948443ec1
Dede3kurniawan <dedetrikurniawann@gmail.com>
2023-06-19 18:22:47 +0700

    Update@19/06/2023/18.22

---
+++ app/src/main/java/com/example/myapplication/activity/pengeluaran/DispatchModDispatchListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.activity.pengeluaran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class DispatchModDispatchListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dispatch_mod_dispatch_list)
+    }
+}
