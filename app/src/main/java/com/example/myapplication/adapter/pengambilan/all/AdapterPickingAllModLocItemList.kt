1ae91ff871dfd3cf6bbc1b93d086ca3e6ed77420
MYhand97 <okka.joker@gmail.com>
2023-06-21 17:14:32 +0700

    update@2023.06.21_17.14

--- app/src/main/java/com/example/myapplication/adapter/pengambilan/all/AdapterPickingAllModLocItemList.kt
+++ app/src/main/java/com/example/myapplication/adapter/pengambilan/all/AdapterPickingAllModLocItemList.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication.adapter.pengambilan.all
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
 import com.example.myapplication.models.pengambilan.all.models.ModelsLocItemList
+import com.example.myapplication.models.pengambilan.all.models.ModelsModLocList
 
 class AdapterPickingAllModLocItemList(var context: Context, var list: List<ModelsLocItemList>?, private val listener: OnAdapterListener)
     : RecyclerView.Adapter<AdapterPickingAllModLocItemList.ItemHolder>(), Filterable {
@@ -14,27 +19,70 @@ class AdapterPickingAllModLocItemList(var context: Context, var list: List<Model
     var tableList : List<ModelsLocItemList> = mutableListOf()
 
     class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        var tvItemID : TextView = itemView.findViewById(R.id.item_id)
+        var tvItemDescription : TextView = itemView.findViewById(R.id.item_description)
+        var tvItemNumber : TextView = itemView.findViewById(R.id.item_number)
+        var tvQty : TextView = itemView.findViewById(R.id.qty)
+        var tvQtyAmbil : TextView = itemView.findViewById(R.id.qty_ambil)
     }
 
     interface OnAdapterListener {
-
+        fun onClick(list: ModelsLocItemList)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
-        TODO("Not yet implemented")
+        val itemHolder = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_picking_mod_loc_item_list, parent, false)
+        return ItemHolder(itemHolder)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list!!.size
     }
 
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
-        TODO("Not yet implemented")
+        val model : ModelsLocItemList = list!![position]
+        holder.tvItemID.text = model.item_id.toString()
+        holder.tvItemDescription.text = model.item_description.toString()
+        holder.tvItemNumber.text = model.item_number.toString()
+        holder.tvQty.text = model.qty.toString()
+        holder.tvQtyAmbil.text = model.qty_ambil.toString()
+        holder.itemView.setOnClickListener {
+            listener.onClick(model)
+        }
     }
 
     override fun getFilter(): Filter {
-        TODO("Not yet implemented")
+        return object : Filter(){
+            override fun performFiltering(p0: CharSequence): FilterResults {
+                val query = p0.toString()
+                val filterResult = FilterResults()
+                filterResult.values =
+                    if(query.isEmpty()){
+                        tableList
+                    }else{
+                        tableList.filter {
+                            it.item_description!!.contains(query, ignoreCase = true) || it.item_description!!.contains(query)
+                            it.item_number!!.contains(query, ignoreCase = true)|| it.item_number!!.contains(query)
+                        }
+                    }
+                return filterResult
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun publishResults(p0: CharSequence?, p1: FilterResults) {
+                list = p1.values as List<ModelsLocItemList>?
+                notifyDataSetChanged()
+            }
+
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setItemList(dataList: MutableList<ModelsLocItemList>){
+        tableList = dataList.toMutableList()
+        list = tableList
+        notifyDataSetChanged()
     }
 
 }
--- app/src/main/java/com/example/myapplication/models/pengambilan/all/models/ModelsLocItemList.kt
+++ app/src/main/java/com/example/myapplication/models/pengambilan/all/models/ModelsLocItemList.kt
@@ -6,6 +6,7 @@ class ModelsLocItemList {
     var loc_name : String? = null
     var item_id : String? = null
     var item_number : String? = null
+    var item_description : String? = null
     var roll_length : String? = null
     var lpn_id : String? = null
     var qty : String? = null
@@ -14,15 +15,16 @@ class ModelsLocItemList {
     var jml_baris : String? = null
     constructor(
         wq_id: String?, from_location: String?, loc_name: String?,
-        item_id: String?, item_number: String?, roll_length: String?,
-        lpn_id: String?, qty: String?, qty_ambil: String?,
-        qty_kurang: String?, jml_baris: String?
+        item_id: String?, item_number: String?, item_description: String?,
+        roll_length: String?, lpn_id: String?, qty: String?,
+        qty_ambil: String?, qty_kurang: String?, jml_baris: String?
     ){
         this.wq_id = wq_id
         this.from_location = from_location
         this.loc_name = loc_name
         this.item_id = item_id
         this.item_number = item_number
+        this.item_description = item_description
         this.roll_length = roll_length
         this.lpn_id = lpn_id
         this.qty = qty
